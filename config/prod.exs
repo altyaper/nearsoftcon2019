bdd47ea4963e576871bd11e281f00cc5ec26425e
Jorge Chavez <jchavz@switchfly.com>
2019-05-16 01:53:34 -0700

    Add host

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :nearsoftcon, NearsoftconWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "gigalixirapp.com", port: 80],
+  url: [host: "ivory-dramatic-steed.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
